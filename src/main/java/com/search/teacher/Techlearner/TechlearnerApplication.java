d23652ccf9ecf48ef8caf5c43010cd3be66342c1
csharpdevelope <drakhimov001@gmail.com>
2023-11-01 11:27:49 +0500

    First initial commit

---
+++ src/main/java/com/search/teacher/Techlearner/TechlearnerApplication.java
@@ -0,0 +1,13 @@
+package com.search.teacher.Techlearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechlearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechlearnerApplication.class, args);
+	}
+
+}
