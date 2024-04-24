a2656d5dcd20ded078b4cbe377e7b4fba819af65
csharpdevelope <drakhimov001@gmail.com>
2024-04-24 14:00:39 +0500

    consume form data

--- src/main/java/com/search/teacher/Techlearner/controller/photo/FileController.java
+++ src/main/java/com/search/teacher/Techlearner/controller/photo/FileController.java
@@ -4,12 +4,9 @@ import com.search.teacher.Techlearner.model.response.JResponse;
 import com.search.teacher.Techlearner.service.upload.ImageService;
 import com.search.teacher.Techlearner.utils.SecurityUtils;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/file/")
